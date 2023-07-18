c8bc59527ed9a3fb74c53d608a51a4e28de3382a
aymene01 <aymene01@hotmail.fr>
2023-07-18 12:08:19 +0200

    setup

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center w-full h-screen">
+      <h1 className="text-5xl font-extralight">.me</h1>
+    </main>
+  )
+}
