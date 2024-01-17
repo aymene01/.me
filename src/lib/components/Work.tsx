68ff32f49c6918da2576ee700cb972bd9dd7f2bc
aymene01 <aymene01@hotmail.fr>
2024-01-17 16:56:14 +0100

    change the project title sectio

--- src/lib/components/Work.tsx
+++ src/lib/components/Work.tsx
@@ -46,7 +46,7 @@ export const Work = () => {
     <div id="work" className="w-full text-gray-300 bg-[#0a192f] py-4">
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full">
         <div className="pb-8">
-          <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600">Current Projects</p>
+          <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600">Projects</p>
         </div>
         <div className="flex flex-wrap justify-start items-stretch space-y-4 md:space-y-0 mt-3">
           {projects.map(project => (
